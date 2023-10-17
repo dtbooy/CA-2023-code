3b7f7111e7bb2808c51f2aae3fdd4756d7d8a434
dtbooy <dtbooy@gmail.com>
2023-10-17 10:24:14 +1000

    added to bankk account challengey

--- python/challenges/bank-account/bank.py
+++ python/challenges/bank-account/bank.py
@@ -2,9 +2,9 @@
 class BankAccount():
    
     #Initialise account with accountholder name and initial deposit
-    def __init__(self, _name, _deposit):
-        self.account_name = _name
-        self.account_balance = _deposit
+    def __init__(self, ac_details, deposit = 0):
+        self.account_holder = Customer(ac_details)
+        self.account_balance = deposit
         
     #balance() returns the current balance of the account
     def balance(self):
@@ -12,22 +12,62 @@ class BankAccount():
     
     # deposit(): accepts an amount and deposits that into the account
     def deposit(self, amount):
-        self.account_balance += amount
+        if amount <= 0:
+            raise ValueError("Withdrawal amount must be positive")
+        else:
+            self.account_balance += amount
+        return amount
 
     # withdraw() accepts an amount and attempts to withdraw it. If 
     # there is enought to cover withdrawal remove from account_balance 
     # & return witdrawal amount
     def withdraw(self, amount):
-        if self.account_balance >= amount:
+        if amount <= 0:
+            raise ValueError("Withdrawal amount must be positive")
+        elif self.account_balance < amount:
+            raise ValueError("Insufficient funds")
+        else:
             self.account_balance -= amount
             return amount
-        return 0
-    
+        
     # transfer() accepts bankac object & amount, transfer if balance 
     # covers it, otherwise do nothing and return 0
     def transfer(self, to_account, amount):
-        if self.account_balance >= amount:
-            self.account_balance -= amount
-            to_account.account_balance += amount
-            return amount
-        return 0
+        return to_account.deposit(self.withdraw(amount))
+        
+
+    # def valid_number_input(self, amount):
+
+    #     else:
+    #         return True
+    
+        # if not isinstance(amount, int) | isinstance(amount, float):
+        #     #  if value is not an integer or a float raise a type error
+        #     try:
+        #         raise TypeError("Invalid input: Amount must be a number")
+        #     except TypeError as message:
+        #         print(message)
+
+# Extend the BankAccount class from last lesson so that, instead 
+# of a string name, it stores an instance of a Customer class that 
+# stores the name, address and date of birth of the customer. Also 
+# abstract Address into it's own class, storing the components of 
+# an address as separate attributes.
+
+class Customer:
+    def __init__(self, ac_details):
+        self.name = ac_details[0]
+        self.address = ac_details[1]
+        self.date_of_birth = ac_details[2]
+
+
+class Address:
+    def __init__(self, st_num, street, st_type, suburb, pcode):
+        self.number = st_num
+        self.street_name = street
+        self.street_type = st_type
+        self.suburb = suburb
+        self.post_code = pcode
+
+    def __str__(self):
+        return f"{self.number} {self.street_name} {self.street_type}, {self.suburb}, {self.post_code}"
--- python/challenges/bank-account/main.py
+++ python/challenges/bank-account/main.py
@@ -2,35 +2,67 @@
 import bank
 
 
-#tests
-acc1 = bank.BankAccount('John Smith', 1000)
-acc2 = bank.BankAccount('Mary Jones', 500)
-
-match input(f""" 
-        hello {acc1.account_name}, what would you like to do?
-        [N]ew account
-        Check [B]alance
-        [W]ithdraw
-        [T]ransfer money
-        """).lower():
-    #Create a new account
-    case "n":
-        new_name = input("Enter acount name: ")
-        new_deposit = int(input("Enter intial deposit: "))
-        acc3 = bank.BankAccount(new_name, new_deposit)
-        print(acc3.__dict__)
-    #check balance
-    case "b":
-        print(f"your account balance is: ${acc1.balance()}")
-    #Withdraw
-    case "w":
-        acc1.withdraw(int(input("How much would you like to withdraw? $")))
-        print(f"your new account balance is {acc1.balance()}")
-    #Transfer 
-    case "t":
-        print("hi")
-    case _:
-        print("invalid selection")
+#Need to define a function to select accounts
+acc1 = bank.BankAccount(["John Smith", "9 some pl", "21/06/1986"], 1000)
+acc2 = bank.BankAccount(["Mary Jones", "2 Penny ln", "18/05/1984"], 500)
+print(f"Hello {acc1.account_holder.name}, what would you like to do?")
+while True:
+    try:
+        match input("""
+        [N]ew account, 
+        [B]alance check, 
+        [D]eposit, 
+        [W]ithdraw, 
+        [T]ransfer money, 
+        [Q]uit
+    """
+            ).lower():
+            
+            #Create a new account
+            case "n" | "new account":
+                # try:
+                    acc_details = [input("Enter acountholder name: ")]
+                    acc_details.append(input("Enter acountholder address: "))
+                    acc_details.append(input("Enter acountholder date of birth: "))
+                    new_deposit = int(input("Enter intial deposit: "))
+                    acc3 = bank.BankAccount(acc_details, new_deposit)
+                    print(acc3.__dict__)
+                # except ValueError as error_msg:
+                    # print(f"Error creating account: {error_msg}.")
+            #check balance
+            case "b" | "balance":
+                print(f"your account balance is: ${acc1.balance()}")
+            #deposit
+            case "d" | "deposit":
+                # try:
+                    acc1.deposit(int(input("How much would you like to deposit? $")))
+                    print(f"your new account balance is {acc1.balance()}")
+                # except ValueError as error_msg:
+                    # print(f"Error creating account: {error_msg}.")
+            #Withdraw
+            case "w" | "withdraw":
+                acc1.withdraw(int(input("How much would you like to withdraw? $")))
+                print(f"your new account balance is {acc1.balance()}")
+            #Transfer 
+            case "t" | "transfer" | "transfer money":
+                # need to add a selection method to assign acc2
+                # try:
+                    acc1.transfer(acc2, int(input(f"How much would you like to transfer to {acc2.account_holder.name}?")))
+                    print(f"your new account balance is {acc1.balance()}")
+                # except ValueError:
+                    # print("Error Transferring Amount: deposit must be a positive number.")
+            #quit function        
+            case "q" | "quit":
+                print("Thank you for Banking!!! Goodbye!")
+                break
+            #invalid selection
+            case _:
+                print("invalid selection, please try again")
+                continue
+             
+    except ValueError as err_msg:
+        print(f"Error: {err_msg}.")
+    input("Press Enter to continue:")
       
 # print(acc1.balance())    # 1000
 # acc1.deposit(100)
