9a40e339b14f5f52bbc542fdfb02706f5ea3c2cc
dtbooy <dtbooy@gmail.com>
2024-01-24 12:34:33 +1000

    FEAT: Add Entry update & delete routes

--- javascript/journal-api/index.js
+++ javascript/journal-api/index.js
@@ -1,25 +1,7 @@
 import express from "express";
-import { EntryModel } from "./db.js"
+import { EntryModel, CategoryModel } from "./db.js";
 
 const categories = ["Food", "Gaming", "Coding", "Other"];
-
-const entries = [
-  { category: "Food", content: "Pizza is ok" },
-  { category: "Food", content: "I liek chocolate milk" },
-  { category: "Gaming", content: "It's all objects" },
-  {
-    category: "Coding",
-    content: "I think I might be able to code something oneday",
-  },
-  {
-    category: "Other",
-    content:
-      "10 things you need to know about rocket rollerblades. I'm going to make this entry reallly really long",
-  },
-  { category: "Other", content: "Are you a Ninja?" },
-];
-
-
 
 // Register app
 const app = express();
@@ -32,8 +14,8 @@ app.get("/", (req, res) => {
   res.status.send({ location: "Home" });
 });
 
-app.get("/categories", (req, res) => {
-  res.status(200).send(categories);
+app.get("/categories", async (req, res) => {
+  res.status(200).send(await CategoryModel.find());
 });
 
 app.get("/entries", async (req, res) => {
@@ -41,8 +23,8 @@ app.get("/entries", async (req, res) => {
 });
 
 /// ":" prefix indicates id is a restful parameter
-app.get("/entries/:id", (req, res) => {
-  const entry = entries[req.params.id - 1];
+app.get("/entries/:id", async (req, res) => {
+  const entry = await EntryModel.findById(req.params.id);
   if (entry) {
     res.send(entry);
   } else {
@@ -50,22 +32,56 @@ app.get("/entries/:id", (req, res) => {
   }
 });
 
+// PostRequest
 app.post("/entries", async (req, res) => {
-  // Get data from request
-  // console.log(req.body);
-  // TO DO: validate
-  //Create new entry object
-  // const newEntry = req.body;
-  // push entry to array!
-  // entries.push(newEntry);
   try {
     const insertedEntry = await EntryModel.create(req.body);
     // res with 201, newEntry
     res.status(201).send(insertedEntry);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    res.status(500).send({ error: err.message });
+  }
+});
+
+// Put Request
+app.put("/entries/:id", async (req, res) => {
+  try {
+    // findByIdAndUpdate
+    const updatedEntry = await EntryModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    // res with 200 (default - .status not required), newEntry
+    if (updatedEntry) {
+      res.send(updatedEntry);
+    } else {
+      res.status(404).send({ error: "Entry not found" });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+
+// DELETE Request
+app.delete("/entries/:id", async (req, res) => {
+  try {
+    // findByIdAndUpdate
+    const updatedEntry = await EntryModel.findByIdAndDelete(
+      req.params.id,
+      req.body
+    );
+    // res with 200 (default - .status not required), newEntry
+    if (updatedEntry) {
+      res.sendStatus(204);
+    } else {
+      res.status(404).send({ error: "Entry not found" });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
   }
 });
 
 // start up app
-app.listen(4001);
+app.listen(4002);
