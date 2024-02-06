9fe7bdfb82918e644b9b5fc73e396182b95b4e47
dtbooy <dtbooy@gmail.com>
2024-02-07 00:41:00 +1000

    Lesson: Added cors Middleware

--- javascript/journal-api/index.js
+++ javascript/journal-api/index.js
@@ -6,23 +6,26 @@
 import express from "express";
 import { CategoryModel } from "./db.js";
 import entryRoutes from "./routes/entry_routes.js"
+import categoryRoutes from "./routes/category_routes.js"
+import cors from "cors"
+
 // Register app
 const app = express();
+
+// Apply Corrs Middleware
+app.use(cors())
 
 // This parses the body of the request into json and saves it in req.body
 app.use(express.json());
 
 // Attach Entry Router Middleware
 app.use("/entries", entryRoutes)
+app.use("/categories", categoryRoutes)
 
 // status 200 is automatic so not required but you can change it like this
 app.get("/", (req, res) => {
   res.status.send({ location: "Home" });
 });
 
-app.get("/categories", async (req, res) => {
-  res.status(200).send(await CategoryModel.find());
-});
-
 // start up app
-app.listen(4002);
+app.listen(4001);
